2bad581affaa854bb46c4ebbfd0c2e92cde443ca
JoaoStipanic <joao.salmeida@sptech.school>
2025-11-19 18:50:19 -0300

    Adicionando as perguntas e o css do quiz

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -2,11 +2,50 @@ CREATE DATABASE tokyodrift;
 
 USE tokyodrift;
 
-CREATE TABLE Usuário (
-	idUsuario INT PRIMARY KEY,
+CREATE TABLE Usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
-    sobrenome VARCHAR(45),
+    sobrenome VARCHAR(45) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    telefone CHAR(11),
+    telefone CHAR(11) NOT NULL,
     senha VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE Quiz (
+    idQuiz INT PRIMARY KEY,
+    titulo VARCHAR(45)
+);
+
+CREATE TABLE Pergunta (
+    idPergunta INT PRIMARY KEY,
+    fkQuiz INT NOT NULL,
+		CONSTRAINT fkQuizPergunta
+			FOREIGN KEY (fkQuiz) REFERENCES Quiz(idQuiz),
+    enunciado VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Resposta (
+    idResposta INT NOT NULL,
+    fkPergunta INT NOT NULL,
+		CONSTRAINT fkPerguntaResposta
+			FOREIGN KEY (fkPergunta) REFERENCES Pergunta(idPergunta),
+		CONSTRAINT pkComposta
+			PRIMARY KEY (idResposta, fkPergunta),
+    opcao CHAR(1) NOT NULL,
+    texto VARCHAR(50) NOT NULL,
+    validacao TINYINT NOT NULL
+);
+
+CREATE TABLE Resultado (
+    idResultado INT,
+    fkUsuario INT,
+    fkQuiz INT,
+    pontuacao INT,
+		CONSTRAINT fkUsuarioResultado
+			FOREIGN KEY (fkUsuario) REFERENCES Usuario(id),
+		CONSTRAINT fkQuizResultado
+			FOREIGN KEY (fkQuiz) REFERENCES Quiz(idQuiz),
+		CONSTRAINT pkComposta
+			PRIMARY KEY (idResultado, fkUsuario, fkQuiz)
+);
+
