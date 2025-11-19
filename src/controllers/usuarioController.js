f2bde7cd0be2f33f7fd1c90ef34c335242f0899f
JoaoStipanic <joao.salmeida@sptech.school>
2025-11-18 23:27:50 -0300

    Configurações iniciais para realizar o login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -20,21 +20,21 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        sobrenome: resultadoAutenticar[0].sobrenome,
-                                        telefone: resultadoAutenticar[0].telefone,
-                                        senha: resultadoAutenticar[0].senha
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                        //     .then((resultadoAquarios) => {
+                        //         if (resultadoAquarios.length > 0) {
+                        //             res.json({
+                        //                 id: resultadoAutenticar[0].id,
+                        //                 email: resultadoAutenticar[0].email,
+                        //                 nome: resultadoAutenticar[0].nome,
+                        //                 sobrenome: resultadoAutenticar[0].sobrenome,
+                        //                 telefone: resultadoAutenticar[0].telefone,
+                        //                 senha: resultadoAutenticar[0].senha
+                        //             });
+                        //         } else {
+                        //             res.status(204).json({ aquarios: [] });
+                        //         }
+                        //     })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
