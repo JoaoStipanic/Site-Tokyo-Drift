0ab849664018f0bb5020a506216f635da5182290
JoaoStipanic <joao.salmeida@sptech.school>
2025-11-21 14:17:10 -0300

    Kpi de média de acertos do usuário logado funcionando

--- src/models/kpiMediaModel.js
+++ src/models/kpiMediaModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 function buscarKpiMedia(idUsuario) {
     var instrucaoSql = `
-        SELECT ROUND(AVG(pontuacao)) AS media FROM Resultado;
+        SELECT ROUND(AVG(pontuacao), 2) AS media FROM Resultado WHERE fkUsuario = ${idUsuario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
